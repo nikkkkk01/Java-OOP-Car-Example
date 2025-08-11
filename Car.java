3b6b41951e7bd9cb182573cce30d24c3313fd95b
n1k <alexaguel367@gmail.com>
2025-08-12 02:03:45 +0800

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,16 @@
+package myPackage;
+
+public class Car {
+
+String brand;
+int year;
+
+void displayInfo() {
+System.out.println("Brand: " + brand);
+System.out.println("Year: " + year);
+}
+
+void startEngine() {
+System.out.println("The engine is now running.");
+}
+}
